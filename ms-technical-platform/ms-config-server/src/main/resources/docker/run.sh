535334aff3591e9739025ef568e68391fb533d59
ZIED ANDOLSI <zied.andolsi-ext@manpower.fr>
2017-11-17 15:44:50 +0100

    add configuration server

--- ms-technical-platform/ms-config-server/src/main/resources/docker/run.sh
+++ ms-technical-platform/ms-config-server/src/main/resources/docker/run.sh
@@ -2,7 +2,4 @@
 echo "-----------------------------"
 echo "Starting Establishment Server"
 echo "-----------------------------"
-s=20
-echo "Waiting $s for configuration server"
-sleep $s
 java -jar /usr/local/services/@project.build.finalName@.jar

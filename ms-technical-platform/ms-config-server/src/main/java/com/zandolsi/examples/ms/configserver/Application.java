3d4abe2621d464dbefbdb7270964f905aa0e889c
ZIED ANDOLSI <zied.andolsi-ext@manpower.fr>
2017-11-21 18:52:29 +0100

    use git as source of configuration server

--- ms-technical-platform/ms-config-server/src/main/java/com/zandolsi/examples/ms/configserver/Application.java
+++ ms-technical-platform/ms-config-server/src/main/java/com/zandolsi/examples/ms/configserver/Application.java
@@ -4,11 +4,62 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.util.Properties;
+
+import static java.util.Objects.nonNull;
+
 @SpringBootApplication
 @EnableConfigServer
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        proxy();
+    }
+
+    private static void proxy() {
+        Properties properties = System.getProperties();
+        ProxyAuthentication passwordAuthentication = new ProxyAuthentication()
+                .username(properties.getProperty("https.proxyUser"))
+                .password(properties.getProperty("https.proxyPassword"));
+        if (nonNull(passwordAuthentication.username()) && nonNull(passwordAuthentication.password())) {
+            Authenticator.setDefault(new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(passwordAuthentication.username(), passwordAuthentication.password().toCharArray());
+                }
+            });
+        }
+    }
+
+    public static class ProxyAuthentication {
+
+        private String username;
+        private String password;
+
+        public String username() {
+            return username;
+        }
+
+        public ProxyAuthentication username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public String password() {
+            return password;
+        }
+
+        public ProxyAuthentication password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "PasswordAuthentication [username=" + username + ", password=" + password + "]";
+        }
+
     }
 }
--- ms-technical-platform/ms-config-server/src/main/resources/docker/run.sh
+++ ms-technical-platform/ms-config-server/src/main/resources/docker/run.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 echo "-----------------------------"
-echo "Starting Establishment Server"
+echo "Starting Configuration Server"
 echo "-----------------------------"
-java -jar /usr/local/services/@project.build.finalName@.jar
+fileName=@project.build.finalName@
+#uncomment this lines if you launch docker behind proxy
+#proxyHost="-Dhttps.proxyHost=server_ip"
+#proxyPort="-Dhttps.proxyPort=server_port"
+#proxyUser="-Dhttps.proxyUser=user"
+#proxyPassword="-Dhttps.proxyPassword=password"
+java $proxyHost $proxyPort $proxyUser $proxyPassword -jar /usr/local/services/$fileName.jar
